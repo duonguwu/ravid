833da481e53dd384eb1f7409d10c1219415d4a02
duongn6 <duongnguyen4823@gmail.com>
2025-07-18 17:24:54 +0700

    chore: update superuser

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -38,7 +38,7 @@ create_superuser() {
 import os
 from django.contrib.auth import get_user_model
 User = get_user_model()
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@ravid.cloud')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
 if not User.objects.filter(email=email).exists():
     User.objects.create_superuser(email=email, password=password)
